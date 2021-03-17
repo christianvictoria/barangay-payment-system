24c7bfaf3a0b2833656cae9370a97fea19192048
christianvictoria <vchristianvictoria@gmail.com>
2021-03-17 20:21:29 +0800

    retrieve data from server using PDO

--- barangay-payment-api/main.php
+++ barangay-payment-api/main.php
@@ -7,21 +7,26 @@
 	$auth = new Auth($pdo);
 
 	if (isset($_REQUEST['request'])) {
-		$req = explode('/', rtrim(base64_decode($_REQUEST['request']), '/'));
+		$req = explode('/', rtrim($_REQUEST['request'], '/'));
 	} else {
 		$req = array("errorcatcher");
 	}
 
 	switch($_SERVER['REQUEST_METHOD']) {
-		case 'POST':
+		case 'GET':
 			switch ($req[0]) {
-				case 'value':
-					# code...
-					break;
+				case 'users':
+					if (count($req) > 1) {
+						echo json_encode($gm->select("SELECT * FROM tbl_users", $req[1]), JSON_PRETTY_PRINT);
+					} else {
+						echo json_encode($gm->select("SELECT * FROM tbl_users", null), JSON_PRETTY_PRINT);
+					}
+				break;
 				
 				default:
-					# code...
-					break;
+					http_response_code(403);
+					echo "Invalid Route/Endpoint";
+				break;
 			}
 
 		break;
--- barangay-payment-api/models/Global.php
+++ barangay-payment-api/models/Global.php
@@ -5,5 +5,20 @@
 		public function __construct(\PDO $pdo) {
 			$this->pdo = $pdo;
 		}
+
+		public function select($sql) {
+			$data = array(); $errmsg = ""; $code = 0;
+			try {
+				if ($res = $this->pdo->query($sql)->fetchAll()) {
+					foreach ($res as $rec) {
+						array_push($data, $rec);
+						$res = null; $code = 200;
+					}
+				}
+			} catch(\PDOException $e) {
+				$errmsg = $e->getMessage(); $code = 401;
+			}
+			return array("code" => $code, "data" => $data, "errmsg" => $errmsg);
+		}
 	}
 ?>
